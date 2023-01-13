bcc4a9100ffa8a19e36d6e5621308ff36cf4de1c
Rabus McCaleb <69118018+rabusmccaleb@users.noreply.github.com>
2023-01-13 16:30:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-document.querySelectorAll('p').forEach(elem => {elem.style.color = 'white';});
-document.querySelector('.overlay-enabled, .mw-body').style.backgroundColor = 'black';
-document.querySelector('.mw-page-title-main').style.color = 'white';
+let ptags = document.querySelectorAll('p');
+if (ptags) {
+  ptags.forEach(elem => {elem.style.color = 'white';});
+}
+
+let bodyTag = document.querySelector('.overlay-enabled, .mw-body');
+if (bodyTag) {
+  bodyTag.style.backgroundColor = 'black';
+}
+
+let title = document.querySelector('.mw-page-title-main');
+if (title) {
+  title.style.color = 'white';
+}
