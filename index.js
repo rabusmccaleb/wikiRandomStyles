1e7975c58fe24a3330d9a0bbf8e46d8bce27c4a9
Rabus McCaleb <69118018+rabusmccaleb@users.noreply.github.com>
2023-01-13 16:25:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 document.querySelectorAll('p').forEach(elem => {elem.style.color = 'white';});
 document.querySelector('.overlay-enabled, .mw-body').style.backgroundColor = 'black';
+document.querySelector('mw-page-title-main').style.color = 'white';
