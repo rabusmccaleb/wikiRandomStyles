24357383c7d4dc706f23b038085e283fff140ce3
Rabus McCaleb <69118018+rabusmccaleb@users.noreply.github.com>
2023-01-14 16:15:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ let title = document.querySelector('.mw-page-title-main');
 if (title) {
   title.style.color = 'white';
 }
+
+let header = document.querySelector('.header-container');
+if (header) {
+  header.style.backgroundColor = 'gray';
+}
+
+let relatedContent = document.querySelector('.ext-related-articles-card-list .ext-related-articles-card');
+if (relatedContent) {
+  relatedContent.backgroundColor = 'gray';
+  let relatedContentTitle = document.querySelector('.ext-related-articles-card-list h3');
+  if (relatedContentTitle) {
+    relatedContentTitle.backgroundImage ='linear-gradient(to right,rgba(255,255,255,0),gray 50%)'
+  }
+}
